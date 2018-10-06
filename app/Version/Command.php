a703c6d8d6eac6ab621fe89fe15d3f35676c65bd
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-10-06 19:43:08 +0300

    Updated the version command

--- app/Version/Command.php
+++ app/Version/Command.php
@@ -3,13 +3,21 @@ namespace App\Version;
 
 use Onion\Framework\Console\Interfaces\CommandInterface;
 use Onion\Framework\Console\Interfaces\ConsoleInterface;
+use Onion\Cli\Manifest\Loader;
 
 class Command implements CommandInterface
 {
+    /** @var Loader $loader */
+    private $loader;
+    public function __construct(Loader $loader)
+    {
+        $this->loader = $loader;
+    }
+
     public function trigger(ConsoleInterface $console): int
     {
-        $version = file_get_contents(__DIR__ . '/../../VERSION');
-        $console->writeLine("%text:cyan%Version %text:green%{$version}");
+        $manifest = $this->loader->getManifest();
+        $console->writeLine("%text:cyan%Version %text:green%{$manifest->getVersion()}");
 
         return 0;
     }
