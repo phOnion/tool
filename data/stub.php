bb5094091aa38e65f66a613efa4bec8cead1ea0a
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-10-09 10:49:16 +0300

    rework stub file to remove the need for indexes

--- data/stub.php
+++ data/stub.php
@@ -1,30 +1,33 @@
 <?php
+use Psr\Http\Message\ServerRequestInterface;
 
 if (!in_array('phar', stream_get_wrappers()) && class_exists('Phar')) {
     fwrite(fopen('php://stderr', 'wb'), 'Phar Extension not available');
     exit(1);
 }
-
 Phar::interceptFileFuncs();
+require_once 'phar://' . __FILE__ . '/vendor/autoload.php';
+
 set_include_path('phar://' . __FILE__ . PATH_SEPARATOR . get_include_path());
+$container = include 'phar://' . __FILE__ . '/container.generated.php';
+$interface = php_sapi_name() === 'cli' ? 'cli' : 'web';
 
-$webIndex = __WEB_STUB__;
-$cliIndex = __CLI_STUB__;
-if ($cliIndex && php_sapi_name() === 'cli') {
-    return include $cliIndex;
-    exit(0);
+$instance = null;
+$args = [];
+if ($interface === 'web') {
+    \Phar::mungServer(['REQUEST_URI','SCRIPT_NAME','SCRIPT_FILENAME','PHP_SELF']);
+    \Phar::webPhar(null, $web);
+    $instance = $container->get(\Onion\Framework\Application\Interfaces\ApplicationInterface::class);
+    $args = [$container->get(ServerRequestInterface::class)];
 }
 
-if ($webIndex) {
-    \Phar::mungServer([
-        'REQUEST_URI',
-        'SCRIPT_NAME',
-        'SCRIPT_FILENAME',
-        'PHP_SELF',
-    ]);
-    \Phar::webPhar(null, $webIndex);
-
-    return include $webIndex;
+if ($interface === 'cli') {
+    $instance = $container->get(\Onion\Framework\Console\Interfaces\ApplicationInterface::class);
+    $args = [$argv ?? [], $container->get(\Onion\Framework\Console\Interfaces\ConsoleInterface::class)];
+}
+if (defined('ONION')) {
+    return $interface;
 }
 
+exit($instance->run(...$args) ?? 0);
 __HALT_COMPILER();
