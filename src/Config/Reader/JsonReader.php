57d646b61a7db632f844ef4a712a6c4d432ea6fc
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-10-29 09:48:01 +0200

    Add support for json with comments

--- src/Config/Reader/JsonReader.php
+++ src/Config/Reader/JsonReader.php
@@ -5,8 +5,13 @@ class JsonReader implements ReaderInterface
 {
     public function parseFile(string $filename): array
     {
-        return json_decode(file_get_contents(
+        return json_decode($this->getJsonContents($filename), true);
+    }
+
+    private function getJsonContents(string $filename): string
+    {
+        return preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t]//.*)|(^//.*)#", '', file_get_contents(
             $filename
-        ), true);
+        ));
     }
 }
