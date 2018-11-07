c94b1344dfe656ad978687c295cf0c56d2484095
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-11-07 04:00:43 +0200

    Use composer for standalone apps but not modules

--- data/standalone.php
+++ data/standalone.php
@@ -5,27 +5,16 @@ if (!in_array('phar', stream_get_wrappers()) && class_exists('Phar')) {
     fwrite(fopen('php://stderr', 'wb'), 'Phar Extension not available');
     exit(1);
 }
+
+if (file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    Phar::mount('vendor/composer.php', __DIR__ . '/../../vendor/autoload.php');
+} else if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    Phar::mount('vendor/composer.php', __DIR__ . '/../vendor/autoload.php');
+} else {
+    Phar::mount('vendor/composer.php', 'phar://' . __FILE__ . '/vendor/autoload.php');
+}
 Phar::interceptFileFuncs();
-// https://www.php-fig.org/psr/psr-4/examples/
-spl_autoload_register(function ($class) {
-    $composer = json_decode(file_get_contents(__DIR__ . '/composer.json'), true);
-    $autoload = $composer['autoload']['psr-4'] ?? [];
-
-    foreach ($autoload as $prefix => $path) {
-        $base_dir = __DIR__ . "/{$path}/";
-        $len = strlen($prefix);
-        if (strncmp($prefix, $class, $len) !== 0) {
-            return;
-        }
-
-        $relative_class = substr($class, $len);
-        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-        if (file_exists($file)) {
-            require $file;
-        }
-    }
-});
+require_once 'phar://' . __FILE__ . '/vendor/composer.php';
 
 set_include_path('phar://' . __FILE__ . PATH_SEPARATOR . get_include_path());
 $container = include 'phar://' . __FILE__ . '/container.generated.php';
