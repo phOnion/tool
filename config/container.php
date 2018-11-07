3729fa33ccdbfa4e4eb5081720d826703aaebd2e
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-11-07 02:47:03 +0200

    Allow loading of modules inside main conainer file

--- config/container.php
+++ config/container.php
@@ -2,11 +2,22 @@
 
 use Onion\Framework\Dependency\Container;
 use function Onion\Framework\merge;
+use Onion\Framework\Dependency\DelegateContainer;
 
 $config = include __DIR__ . '/config.global.php';
 $console = include __DIR__ . '/console.global.php';
 $container = include __DIR__ . '/container.global.php';
-
 $cfg = merge($config, merge($console, $container));
 
-return new Container($cfg);
+$container = new Container($cfg);
+
+if (file_exists(__DIR__ . '/modules.global.php')) {
+    $modules = include __DIR__ . '/modules.global.php';
+    $containers = [$container];
+    foreach ($modules['modules'] as $file) {
+        $containers[] = include __DIR__ . '/../' . $file;
+    }
+
+    $container = new DelegateContainer($containers);
+}
+return $container;
