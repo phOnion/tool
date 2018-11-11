8f401277a371f0e27df44f9101abd9fbd4942461
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-11-11 14:31:38 +0200

    Update cli index to match standalone stub

--- bin/cli.php
+++ bin/cli.php
@@ -4,12 +4,43 @@ use Onion\Framework\Console\Console;
 use Psr\Container\ContainerInterface;
 use Onion\Console\Application\Application;
 use Onion\Framework\Console\Interfaces\ConsoleInterface;
+use Onion\Framework\Dependency\DelegateContainer;
+use Psr\Http\Message\ServerRequestInterface;
 
 /** @var ContainerInterface $container */
 $container = include __DIR__ . '/../config/container.php';
-$console = $container->get(ConsoleInterface::class);
+$containers = [$container];
+foreach ([getcwd(), __DIR__] as $dir) {
+    if (is_dir("{$dir}/modules/")) {
+        $iterator = new \RegexIterator(new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+                "{$dir}/modules/",
+                \FilesystemIterator::CURRENT_AS_PATHNAME | \FilesystemIterator::SKIP_DOTS
+            )
+        ), '~\.phar$~', \RegexIterator::MATCH, \RegexIterator::USE_KEY);
 
-$container->get(Application::class)->run(
-    $argv,
-    $console
-);
+        foreach ($iterator as $item) {
+            $containers[] = include $item;
+        }
+    }
+}
+$container = new DelegateContainer($containers);
+$interface = php_sapi_name() === 'cli' ? 'cli' : 'web';
+
+$instance = null;
+$args = [];
+if ($interface === 'web') {
+    $instance = $container->get(\Onion\Framework\Application\Interfaces\ApplicationInterface::class);
+    $args = [$container->get(ServerRequestInterface::class)];
+}
+
+if ($interface === 'cli') {
+    $instance = $container->get(\Onion\Framework\Console\Interfaces\ApplicationInterface::class);
+    $args = [$argv ?? [], $container->get(\Onion\Framework\Console\Interfaces\ConsoleInterface::class)];
+}
+
+if (defined('ONION')) {
+    return $instance;
+}
+
+exit($instance->run(...$args) ?? 0);
