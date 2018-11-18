22654ad7e7004dd7034afae2f8849b6358c17cd1
Dimitar Dimitrov <daghostman.dd@gmail.com>
2018-11-18 17:17:00 +0200

    Modules will no-longer have a stub

--- data/module.php
+++ data/module.php
@@ -1,11 +1,6 @@
 <?php
 use Psr\Http\Message\ServerRequestInterface;
 
-if (!in_array('phar', stream_get_wrappers()) && class_exists('Phar')) {
-    fwrite(fopen('php://stderr', 'wb'), 'Phar Extension not available');
-    exit(1);
-}
-Phar::interceptFileFuncs();
 // https://www.php-fig.org/psr/psr-4/examples/
 spl_autoload_register(function ($class) {
     $composer = json_decode(file_get_contents('phar://' . __FILE__ . '/composer.json'), true);
@@ -27,7 +22,5 @@ spl_autoload_register(function ($class) {
     }
 }, false, true);
 
-set_include_path('phar://' . __FILE__ . PATH_SEPARATOR . get_include_path());
 return include 'phar://' . __FILE__ . '/container.generated.php';
 
-__HALT_COMPILER();
