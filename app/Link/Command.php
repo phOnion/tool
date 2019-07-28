69e58170c16fb1fca70c89586cecd88b491572ba
DaGhostman <daghostman.dd@gmail.com>
2019-07-28 21:01:24 +0300

    add option to delete links

--- app/Link/Command.php
+++ app/Link/Command.php
@@ -2,6 +2,7 @@
 namespace Onion\Tool\Link;
 
 use Onion\Cli\Manifest\Entities\Link;
+use Onion\Cli\Manifest\Entities\Manifest;
 use Onion\Cli\Manifest\Loader;
 use Onion\Framework\Console\Interfaces\CommandInterface;
 use Onion\Framework\Console\Interfaces\ConsoleInterface;
@@ -11,6 +12,7 @@ class Command implements CommandInterface
     private const ALLOWED_ACTIONS = [
         'list',
         'add',
+        'delete',
     ];
     /** @var Loader $loader */
     private $loader;
@@ -31,18 +33,7 @@ class Command implements CommandInterface
                 );
                 break;
             case 'list':
-                foreach ($manifest->getLinks() as $index => $link) {
-                    /** @var Link $link */
-                    $lang = $link->getLang();
-                    if ($lang !== '') {
-                        $lang = "%text:cyan%{$lang}%text:green% -";
-                    }
-
-                    $index++;
-                    $console->writeLine(
-                        "{$index}) %text:green%{$link->getTitle()} - {$lang}%text:white%{$link->getHref()}"
-                    );
-                }
+                $this->listLinks($console, $manifest);
                 break;
             case 'add':
                 $manifest = $manifest->addLink(
@@ -55,8 +46,53 @@ class Command implements CommandInterface
 
                 $this->loader->saveManifest(getcwd(), $manifest);
                 break;
+            case 'delete':
+                if (count($manifest->getLinks()) === 0) {
+                    $console->writeLine('%text:yellow%No links available');
+                    return 1;
+                }
+
+                $this->listLinks($console, $manifest);
+                $index = (int) $console->prompt("%text:cyan%Index of list to delete");
+                $links = $manifest->getLinks();
+
+                foreach ($links as $i => $link) {
+                    if ($i+1 === $index) {
+                        $confirm = $console->confirm(
+                            "%text:yellow%Do you want to delete: {$link->getTitle()} ({$link->getHref()}",
+                            'n'
+                        );
+
+                        if ($confirm) {
+                            unset($links[$i]);
+                            $this->loader->saveManifest(getcwd(), $manifest->withLinks($links));
+                        }
+
+                        return 0;
+                    }
+                }
+
+                $console->writeLine("%text:yellow%No links removed");
+                return 1;
+                break;
         }
 
         return 0;
+    }
+
+    private function listLinks(ConsoleInterface $console, Manifest $manifest)
+    {
+        foreach ($manifest->getLinks() as $index => $link) {
+            /** @var Link $link */
+            $lang = $link->getLang();
+            if ($lang !== '') {
+                $lang = "%text:cyan%{$lang}%text:green% -";
+            }
+
+            $index++;
+            $console->writeLine(
+                "{$index}) %text:green%{$link->getTitle()} - {$lang}%text:white%{$link->getHref()}"
+            );
+        }
     }
 }
