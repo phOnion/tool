df18f54a9533511d12955c01016e6c4111fa2f72
DaGhostman <daghostman.dd@gmail.com>
2019-08-13 18:00:53 +0300

    centralize autoloader

---
+++ data/loader.php
@@ -0,0 +1,54 @@
+<?php
+
+$base = __DIR__;
+$autoload = [];
+if (file_exists("{$base}/autoload.generated.php")) {
+    $autoload = include "{$base}/autoload.generated.php";
+
+    if (isset($autoload['files'])) {
+        foreach ($autoload['files'] as $file) {
+            $file = "{$base}/{$file}";
+            if (!file_exists($file)) {
+                continue;
+            }
+
+            include_once $file;
+        }
+
+        unset($autoload['files']);
+    }
+}
+
+spl_autoload_register(function ($class) use ($autoload, $base) {
+    $parts = explode('\\', $class);
+
+    $segment = '';
+    foreach ($parts as $part) {
+        $segment .= "{$part}\\";
+
+        foreach ($autoload as $type => $definitions) {
+            if (isset($definitions[$segment])) {
+                foreach ($definitions[$segment] as $path) {
+                    $relative_class = $class;
+                    $len = 0;
+
+                    if ($type === 'psr-4') {
+                        $len = strlen($segment);
+                        $relative_class = substr($class, $len);
+                    }
+
+                    $path = trim($path, '\/');
+                    $file = "{$base}/{$path}/" . str_replace('\\', '/', $relative_class) . '.php';
+
+                    if (strncmp($segment, $class, $len) !== 0 || !file_exists($file)) {
+                        continue;
+                    }
+
+                    include_once $file;
+
+                    return true;
+                }
+            }
+        }
+    }
+}, false, true);
