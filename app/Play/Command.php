191c3a020cbf5107f29cab20df963abe73d2900c
Dimitar Dimitrov <daghostman.dd@gmail.com>
2019-08-29 17:58:23 +0300

    update to match interface requirements

--- app/Play/Command.php
+++ app/Play/Command.php
@@ -19,7 +19,7 @@ class Command implements CommandInterface
         $this->loader = $loader;
     }
 
-    public function trigger(ConsoleInterface $console)
+    public function trigger(ConsoleInterface $console): int
     {
         $config = $console->getArgument('config');
         $configLocation = getcwd() . '/' . $config;
