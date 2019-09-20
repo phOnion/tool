cd3fbef6621d7394ea2219081779910741f06e0b
DaGhostman <daghostman.dd@gmail.com>
2019-09-20 16:38:34 +0300

    improve play command to asynchronously run each step

--- app/Play/Command.php
+++ app/Play/Command.php
@@ -5,12 +5,14 @@ use function Onion\Framework\Loop\coroutine;
 
 use Onion\Cli\Watcher\Watcher;
 use Onion\Framework\Common\Config\Loader;
+use Onion\Framework\Console\Components\Progress;
 use Onion\Framework\Console\Interfaces\CommandInterface;
 
 use Onion\Framework\Console\Interfaces\ConsoleInterface;
-use Onion\Framework\Loop\Interfaces\ResourceInterface;
+use Onion\Framework\Loop\Coroutine;
 use Onion\Framework\Loop\Timer;
 use Onion\Framework\Process\Process;
+use Onion\Framework\Promise\FulfilledPromise;
 use Onion\Framework\State\Flow;
 use Onion\Framework\State\Transition;
 use Phar;
@@ -41,21 +43,16 @@ class Command implements CommandInterface
             }
 
             $definition = &$config['stages'][$stage];
-            yield from $this->runCommands(
-                $console,
-                $definition,
-                $config,
-                $stage
-            );
+            try {
+                yield from $this->runCommands(
+                    $console,
+                    $definition,
+                    $config,
+                    $stage
+                );
 
-            if (isset($definition['watch'])) {
-                coroutine(function (
-                    int $interval,
-                    ConsoleInterface $console,
-                    array $definition,
-                    array $config,
-                    string $stage
-                ) {
+
+                if (isset($definition['watch']) && !$console->getArgument('no-watch')) {
                     $handler = function () use ($console, $definition, $config, $stage) {
                         coroutine(function ($console, $definition, $config, $stage) {
                             try {
@@ -68,8 +65,7 @@ class Command implements CommandInterface
                             } catch (\Throwable $ex) {
                                 //
                             }
-                            $console->writeLine("\e[H\e[J");
-                            $console->writeLine("%text:cyan%Watching for changes");
+
                         }, [$console, $definition, $config, $stage]);
                     };
                     $flow = new Flow('watcher', 'initial');
@@ -78,7 +74,6 @@ class Command implements CommandInterface
                     $flow->addTransition(new Transition('change', 'delete'));
 
                     $watcher = new Watcher($flow);
-                    $console->writeLine("%text:cyan%Watching for changes");
 
                     yield Timer::interval(function (Watcher $watcher, array $definition) {
                         foreach ((array) $definition['watch'] as $dir) {
@@ -87,17 +82,11 @@ class Command implements CommandInterface
                             } catch (\Throwable $ex) {
                                 // Prevent crashes
                             }
-
                         }
-                        usleep(250000);
-                    }, $interval, [$watcher, $definition]);
-                }, [
-                    $console->getArgument('interval', 1000),
-                    $console,
-                    $definition,
-                    $config,
-                    $stage
-                ]);
+                    }, (int) $console->getArgument('interval', 1000), [$watcher, $definition]);
+                }
+            } catch (\Throwable $ex) {
+                exit(1);
             }
         }, [$this->loader->loadFile($configLocation), $console]);
 
@@ -114,53 +103,79 @@ class Command implements CommandInterface
 
     private function runCommands($console, $definition, $config, $stage)
     {
+        // microtime(true);
         if (isset($definition['require'])) {
             $deps = (array) $definition['require'];
             foreach ($deps as $dep) {
+                $console->write("%text:cyan%Executing dependency task %text:blue%{$dep}");
+
                 $process = $this->execProcess(
                     Phar::running(false) ?: "php {$_SERVER['PHP_SELF']}",
-                    ['play', $dep]
+                    ['play', $dep, '--no-watch']
                 );
 
                 do {
-                    $console->write($process->read(1024));
+                    yield $process->wait();
+
+                    $process->read(1024);
                     if (!$process->isAlive()) {
                         break;
                     }
-                    usleep(250000);
-                    yield $process->wait();
-
                 } while ($process->isRunning());
 
                 if ($process->getExitCode() !== 0) {
-                    throw new \RuntimeException("Required stage {$dep} failed");
+                    $console->writeLine("%text:red%Required stage '%text:bold-red%{$dep}%text:red%' failed");
+                    throw new \RuntimeException('Required step failed');
                 }
+                $console->writeLine("%text:bold-green%\t\tDONE");
             }
         }
 
-        $console->writeLine("\n\t%text:bold-white%STAGE %text:green%{$stage}");
+        $console->write(
+            "%text:cyan%Executing stage %text:green%{$stage}"
+        );
+
+        $results = [];
         foreach ($config['stages'][$stage]['steps'] ?? [] as $index => $step) {
-            $number = $index+1;
-            $console->write("%text:cyan%Step %text:yellow%#{$number}");
-            $process = $this->execProcess(
-                $step['command'],
-                $step['args'] ?? [],
-                $step['cwd'] ?? getcwd(),
-                $step['env'] ?? []
-            );
-
-            while ($process->isRunning()) {
-                yield $process->wait();
-                $process->read(1024);
-            }
-
-            if ($process->getExitCode() !== 0) {
-                $console->writeLine(
-                    "\t\t%text:bold-red%FAIL (Error code {$process->getExitCode()})"
+            yield Coroutine::create(function ($console, $stage, $index, $step) use (&$results) {
+                $process = $this->execProcess(
+                    $step['command'],
+                    $step['args'] ?? [],
+                    $step['cwd'] ?? getcwd(),
+                    $step['env'] ?? []
                 );
-                throw new \RuntimeException("Build of {$stage} failed");
+
+                while ($process->isRunning()) {
+                    yield $process->wait();
+                    $process->read(1024);
+                }
+
+                if ($process->getExitCode() !== 0) {
+                    $index += 1;
+                    $console->writeLine(
+                        "\t%text:bold-red%#{$index} FAIL (Error code {$process->getExitCode()})"
+                    );
+                    throw new \RuntimeException("Build of {$stage} failed");
+                }
+
+                $results[] = true;
+            }, [$console, $stage, $index, $step]);
+        }
+
+        $timer = -1;
+        $timer = yield Timer::interval(function ($console, $definition, $totalSteps) use (&$timer, &$results) {
+            if (count($results) !== $totalSteps) {
+                return;
             }
             $console->writeLine("\t\t%text:bold-green%DONE");
-        }
+
+            yield Coroutine::kill($timer);
+            if (isset($definition['watch']) && !$console->getArgument('no-watch')) {
+                yield Timer::after(function (ConsoleInterface $console) {
+                    yield $console->writeLine("\e[H\e[J");
+                    $console->writeLine("%text:cyan%Watching for changes");
+                }, 250, [$console]);
+            }
+        }, 250, [$console, $definition, count($config['stages'][$stage]['steps'] ?? [])]);
     }
 }
